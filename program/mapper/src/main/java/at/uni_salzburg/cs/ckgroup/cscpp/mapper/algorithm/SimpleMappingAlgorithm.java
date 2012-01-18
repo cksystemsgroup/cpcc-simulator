1269df78f2f1551e65f88cb490b42f233c1fe970
ckrainer <clemens.krainer@gmail.com>
2012-01-18 01:39:25 +0000

    Use the engine local vehicle name as identification for migration. Source formatted. Avoid NPE in isNear().

--- program/mapper/src/main/java/at/uni_salzburg/cs/ckgroup/cscpp/mapper/algorithm/SimpleMappingAlgorithm.java
+++ program/mapper/src/main/java/at/uni_salzburg/cs/ckgroup/cscpp/mapper/algorithm/SimpleMappingAlgorithm.java
@@ -32,107 +32,105 @@ import org.apache.log4j.Logger;
 public class SimpleMappingAlgorithm extends AbstractMappingAlgorithm {
 
 	Logger LOG = Logger.getLogger(SimpleMappingAlgorithm.class);
-        private IGeodeticSystem geodeticSystem = new WGS84();
-        private String central_engine;
+	private IGeodeticSystem geodeticSystem = new WGS84();
+	private String central_engine;
 	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		LOG.error("execute() not yet implemented!");
                 
-                if (virtualVehicleMap.isEmpty()) {
+		if (virtualVehicleMap.isEmpty()) {
 			LOG.info("No migration because of empty virtual vehicle map.");
 			return;
 		}
                 
-                if(statusProxyMap.isEmpty()) {
-                        LOG.info("No migration because of empty status proxy map.");
-                        return;                        
-                }
+		if(statusProxyMap.isEmpty()) {
+			LOG.info("No migration because of empty status proxy map.");
+			return;
+		}
                 
-                
-                
-                for (Map.Entry<String, Map<String, VehicleStatus>> vehicle : virtualVehicleMap.entrySet()) {
-                    
-                    if(vehicle == null)
-                        continue;
-                    else if(vehicle.getValue().isEmpty())
-                        continue;
-                    else {
-                        
-                        String eng_url = vehicle.getKey();
-                        Map<String, VehicleStatus> vstat_map = vehicle.getValue();
-                        
-                        for(Map.Entry<String, VehicleStatus> vstat : vstat_map.entrySet()) {
-                                VehicleStatus vs = (VehicleStatus) vstat.getValue();
-                                String vv = vs.getId();
+		for (Map.Entry<String, Map<String, VehicleStatus>> vehicle : virtualVehicleMap.entrySet()) {
+		    
+			if(vehicle == null)
+				continue;
+			else if(vehicle.getValue().isEmpty())
+				continue;
+			else {
+				String eng_url = vehicle.getKey();
+				Map<String, VehicleStatus> vstat_map = vehicle.getValue();
 
-                                if(vs.getState() == Status.COMPLETED) {
-                                    if(central_engine != null) {
-                                        migrate(eng_url, vv, central_engine);
-                                    }
-                                    continue;
-                                }
-                                
-                                String[] actions = vs.getActions();
-                                PolarCoordinate pos = vs.getPosition();
-                                double tol = vs.getTolerance();
-                                
-                                String eng_uri_new = getEngine(pos, tol, actions);
-                                
-                                if(eng_uri_new == null) {
-                                    continue;
-                                }
-                                
-                                if(eng_uri_new.equalsIgnoreCase(eng_url)) {
-                                    continue;
-                                }
-                                else {
-                                    migrate(eng_url, vv, eng_uri_new);
-                                }
-                        }
-                     
-                    }
-                }
+				for (Map.Entry<String, VehicleStatus> vstat : vstat_map.entrySet()) {
+					VehicleStatus vs = (VehicleStatus) vstat.getValue();
+					String vv = vs.getName();
+
+					if (vs.getState() == Status.COMPLETED) {
+						if (central_engine != null) {
+							migrate(eng_url, vv, central_engine);
+						}
+						continue;
+					}
+
+					String[] actions = vs.getActions();
+					PolarCoordinate pos = vs.getPosition();
+					double tol = vs.getTolerance();
+
+					String eng_uri_new = getEngine(pos, tol, actions);
+
+					if (eng_uri_new == null) {
+						continue;
+					}
+
+					if (eng_uri_new.equalsIgnoreCase(eng_url)) {
+						continue;
+					} else {
+						migrate(eng_url, vv, eng_uri_new);
+					}
+				}
+
+			}
+		}
 	}
         
-        private String getEngine(PolarCoordinate pos, double tol, String[] sensors) {
-            CartesianCoordinate virtualPosCart = geodeticSystem.polarToRectangularCoordinates(pos);
-            
-            for(Map.Entry<String,StatusProxy> sp :statusProxyMap.entrySet()) {
-                String v = sp.getKey();
-                StatusProxy s = sp.getValue();
-                PolarCoordinate cur = s.getCurrentPosition();
-                
-                if(cur != null) {
-                    CartesianCoordinate currentPosCart = geodeticSystem.polarToRectangularCoordinates(cur);
-                    CartesianCoordinate nextPosCart = geodeticSystem.polarToRectangularCoordinates(s.getNextPosition());
-                    if(isNear(currentPosCart, nextPosCart, virtualPosCart, tol)) {
-                        RegData re_val = registrationData.get(v);
-                        if(re_val != null) {
-                            String[] sens_v = (String[]) re_val.getSensors().toArray();
-                            for(int i = 0; i < sens_v.length; i++) {
-                                if(sensors.toString().contains(sens_v[i]))
-                                    return v;
-                            }
-                        }
-                    }    
-                }
-                else if (central_engine == null) central_engine = v;
-            }
-            return null;
-        }
+    private String getEngine(PolarCoordinate pos, double tol, String[] sensors) {
+    	if (pos == null) {
+    		return null;
+    	}
+        CartesianCoordinate virtualPosCart = geodeticSystem.polarToRectangularCoordinates(pos);
         
-        private boolean isNear(CartesianCoordinate current, CartesianCoordinate next, CartesianCoordinate point, double tol) {
-            // direction vec = next - current
-            CartesianCoordinate dir = next.subtract(current);
-            // d = |dir x (point - current)| / |dir|
-            double d = (dir.crossProduct((current.subtract(point))).norm()) / dir.norm();
+        for(Map.Entry<String,StatusProxy> sp :statusProxyMap.entrySet()) {
+            String v = sp.getKey();
+            StatusProxy s = sp.getValue();
+            PolarCoordinate cur = s.getCurrentPosition();
+            PolarCoordinate nxt = s.getNextPosition();
             
-            if(d <= tol)
-                return true;
-            else
-                return false;
+            if(cur != null && nxt != null) {
+                CartesianCoordinate currentPosCart = geodeticSystem.polarToRectangularCoordinates(cur);
+                CartesianCoordinate nextPosCart = geodeticSystem.polarToRectangularCoordinates(nxt);
+                if(isNear(currentPosCart, nextPosCart, virtualPosCart, tol)) {
+                    RegData re_val = registrationData.get(v);
+                    if(re_val != null) {
+                        String[] sens_v = re_val.getSensors().toArray(new String[0]);
+                        for(int i = 0; i < sens_v.length; i++) {
+                            if(sensors.toString().contains(sens_v[i]))
+                                return v;
+                        }
+                    }
+                }    
+            }
+            else if (central_engine == null) central_engine = v;
         }
+        return null;
+    }
+        
+    private boolean isNear(CartesianCoordinate current, CartesianCoordinate next, CartesianCoordinate point, double tol) {
+        // direction vec = next - current
+        CartesianCoordinate dir = next.subtract(current);
+        // d = |dir x (point - current)| / |dir|
+        double d = (dir.crossProduct((current.subtract(point))).norm()) / dir.norm();
+        
+        if(d <= tol)
+            return true;
+        else
+            return false;
+    }
 	
 }

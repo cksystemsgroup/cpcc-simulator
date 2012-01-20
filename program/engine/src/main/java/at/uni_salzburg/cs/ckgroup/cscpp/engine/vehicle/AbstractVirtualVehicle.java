19b842f766f351042edc8a0e3aaaac529fc15aef
ckrainer <clemens.krainer@gmail.com>
2012-01-20 01:01:57 +0000

    Catch all exceptions of execute() to maintain a correct running variable.

--- program/engine/src/main/java/at/uni_salzburg/cs/ckgroup/cscpp/engine/vehicle/AbstractVirtualVehicle.java
+++ program/engine/src/main/java/at/uni_salzburg/cs/ckgroup/cscpp/engine/vehicle/AbstractVirtualVehicle.java
@@ -1,7 +1,7 @@
 /*
  * @(#) AbstractVirtualVehicle.java
  *
- * This code is part of the JNavigator project.
+ * This code is part of the ESE CPCC project.
  * Copyright (c) 2011  Clemens Krainer
  *
  * This program is free software; you can redistribute it and/or modify
@@ -43,7 +43,7 @@ import at.uni_salzburg.cs.ckgroup.cscpp.utils.ISensorProxy;
 
 public abstract class AbstractVirtualVehicle implements IVirtualVehicle, Runnable {
 	
-	Logger LOG = Logger.getLogger(AbstractVirtualVehicle.class);
+	private static final Logger LOG = Logger.getLogger(AbstractVirtualVehicle.class);
 	
 	public static final String PROGRAM_PATH = "vehicle.prg";
 	public static final String LOG_PATH = "vehicle.log";
@@ -96,7 +96,7 @@ public abstract class AbstractVirtualVehicle implements IVirtualVehicle, Runnabl
 	/**
 	 * True if the currently loaded program is running.
 	 */
-	private boolean running = false;
+	private volatile boolean running = false;
 	
 	/**
 	 * The proxy instance to access sensor data of real vehicles.
@@ -116,13 +116,13 @@ public abstract class AbstractVirtualVehicle implements IVirtualVehicle, Runnabl
 	/**
 	 * True if all tasks of this virtual vehicle have been successful.
 	 */
-	private boolean completed = false;
+	private volatile boolean completed = false;
 	
 	/**
 	 * True this virtual vehicle is frozen, i.e., suspended and invisible to the
 	 * mapper.
 	 */
-	private boolean frozen;
+	private volatile boolean frozen;
 	
 	/**
 	 * Construct a virtual vehicle instance.
@@ -231,7 +231,11 @@ public abstract class AbstractVirtualVehicle implements IVirtualVehicle, Runnabl
 		running = true;
 		
 		if (sensorProxy != null) {
-			execute();
+			try {
+				execute();
+			} catch (Throwable t) {
+				LOG.error("Virtual vehicle command failed.", t);
+			}
 		}
 		
 		running = false;

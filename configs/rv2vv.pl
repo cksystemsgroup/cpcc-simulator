5eda81b5dcc37fa4dfdfc6b76c82cd8ba8bba39c
ckrainer <clemens.krainer@gmail.com>
2012-07-08 20:19:12 +0000

    Match virtual vehicle courses containing blanks in their name.

--- configs/rv2vv.pl
+++ configs/rv2vv.pl
@@ -8,7 +8,7 @@ use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
 
 my $name = undef;
 my @prg = map { sprintf "Point %s %s %s tolerance 5 Picture Temperature Airpressure\n", @$_ }
-grep {$_} map { m/## \@\(#\) set course (\S+).*/ and $name = $1; m/fly to\((\d+\.?\d*),(\d+\.?\d*),(\d+\.?\d*)\).*/i and $_ = [$1,$2,$3] or undef } <>;
+grep {$_} map { m/## \@\(#\) set course (\S+)(\s+\S+)?.*/ and $name = $1.$2; m/fly to\((\d+\.?\d*),(\d+\.?\d*),(\d+\.?\d*)\).*/i and $_ = [$1,$2,$3] or undef } <>;
 
 my $zip = Archive::Zip->new();
 my $string_member = $zip->addString( join('',@prg), 'vehicle.prg' );
@@ -20,6 +20,7 @@ $string_member->desiredCompressionMethod( COMPRESSION_DEFLATED );
 
 
 my $file = $name.'.zip';
+$file =~ s/\s+/-/g;
 $zip->writeToFileNamed($file) == AZ_OK or die "Can not write $file";
 print "File $file written successfully\n".
 
